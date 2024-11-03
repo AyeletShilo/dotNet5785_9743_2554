2ee6e11cfc0aa89d90f9fbcfdf44de8640cea7d2
איילת שילה <ashilo@g.jct.ac.il>
2024-11-03 16:07:23 +0200

    I add a new program and I invite you to join:)

---
+++ stage0/program9743.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static partial void Welcome9743()
+        {
+            console.WriteLine("I am hereee");
+        }
+    
+}
