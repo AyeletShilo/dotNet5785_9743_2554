d277a5ca86854caf3d13a1785dd4aac320cdbfdc
tehilakufeld <tehilakufeld1@gmail.com>
2024-11-03 15:32:36 +0200

    I invited you to visit my code:)

---
+++ stage0/Program2554.cs
@@ -0,0 +1,20 @@
+﻿//Console.WriteLine("Hello, World!");
+
+
+partial class Program
+{
+    private static void Main(string[] args)
+    {
+        welcome2554();
+        welcome9743();
+        Console.ReadKey();
+    }
+
+    static partial void welcome9743();
+    private static void welcome2554()
+    {
+        Console.WriteLine("Enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+}
