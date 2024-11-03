4860186aa9e7e30f1f059ff6114d20ee7c3fa9b8
tehilakufeld <tehilakufeld1@gmail.com>
2024-11-03 15:56:15 +0200

    with namespace

--- stage0/Program2554.cs
+++ stage0/Program2554.cs
@@ -1,20 +1,23 @@
 ﻿//Console.WriteLine("Hello, World!");
 
+namespace Targil0
 
-partial class Program
 {
-    private static void Main(string[] args)
+    partial class Program
     {
-        welcome2554();
-        welcome9743();
-        Console.ReadKey();
-    }
+        private static void Main(string[] args)
+        {
+            welcome2554();
+            welcome9743();
+            Console.ReadKey();
+        }
 
-    static partial void welcome9743();
-    private static void welcome2554()
-    {
-        Console.WriteLine("Enter your name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine("{0}, welcome to my first console application", name);
+        static partial void welcome9743();
+        private static void welcome2554()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
     }
 }
