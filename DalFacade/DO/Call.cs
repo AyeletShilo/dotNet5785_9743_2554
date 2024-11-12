1ced3bcbcef0a45a627cac633f0b9a88f9149712
איילת שילה <ashilo@g.jct.ac.il>
2024-11-12 21:12:48 +0200

    chapter 6

--- DalFacade/DO/Call.cs
+++ DalFacade/DO/Call.cs
@@ -1,8 +1,17 @@
-﻿
-using static System.Runtime.InteropServices.JavaScript.JSType;
+﻿namespace DO;
 
-namespace DO;
-
+/// <summary>
+/// Contains details for a call opened by an administrator, 
+/// and handled by a volunteer,including a running ID number
+/// </summary>
+/// <param name="Id">Personal unique ID of the call</param>
+/// <param name="callType">Types of calls received in the organization</param>
+/// <param name="callAddress">Full and real address in correct format, of the call location</param>
+/// <param name="Latitude">A number indicating how far a point on Earth is south or north of the equator</param>
+/// <param name="Longitude">A number indicating how far a point on Earth is east or west of the equator</param>
+/// <param name="OpenTime">Time (date and hour) when the call was opened by the manager.</param>
+/// <param name="description">Description of the call</param>
+/// <param name="MaxTime">Time (date and hour) by which the call should be closed.</param>
 public record Call
 (
    int Id,//?//
--- DalFacade/DO/Volunteer.cs
+++ DalFacade/DO/Volunteer.cs
@@ -1,6 +1,19 @@
 ﻿
 namespace DO;
-
+/// <summary>
+/// Details for a manager or volunteer in the organization, including a unique ID number
+/// </summary>
+/// <param name="Id">A standard ID that uniquely identifies the volunteer.</param>
+/// <param name="FullName">Full name of the volunteer</param>
+/// <param name="PhoneNumber">Standard cell phone of the volunteer. 10 digits only. Starts with the number 0.</param>
+/// <param name="Email">Email address is valid in terms of format</param>
+/// <param name="Job">Role of the volunteer</param>
+/// <param name="Active">Whether the volunteer is active or not</param>
+/// <param name="Distance">Type of distance: aerial distance, walking distance, driving distance</param>
+/// <param name="VolAddress">Full and real address in correct format, of the volunteer. where it is currently located and available to receive calls</param>
+/// <param name="Latitude">A number indicating how far a point on Earth is south or north of the equator</param>
+/// <param name="Longitude">A number indicating how far a point on Earth is east or west of the equator</param>
+/// <param name="MaxDistance">The maximum distance for receiving a call that the each volunteer chose</param>
 public record Volunteer
 (
     int Id,
@@ -15,7 +28,7 @@ public record Volunteer
     double? Latitude = null,
     double? Longitude =null,
     double? MaxDistance=null
-    )
+)
     
 
 {
