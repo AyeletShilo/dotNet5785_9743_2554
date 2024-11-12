fb0988bb806735924a426fdfcbab7bd26153f308
tehilakufeld <tehilakufeld1@gmail.com>
2024-11-12 17:36:57 +0200

    finish Assignment

--- DalFacade/DO/Assignment.cs
+++ DalFacade/DO/Assignment.cs
@@ -7,7 +7,9 @@ public record Assignment
     int CallId,
     int VolunteerId,
     DateTime InterTime,
-    DateTime? EndTime=null
+    DateTime? EndTime=null,
+    AssignmentEnum? EndTreatment = null
  )
 {
+    public Assignment():this(0,0,0,DateTime.MinValue) {}
 }
