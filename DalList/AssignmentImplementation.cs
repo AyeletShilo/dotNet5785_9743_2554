bd1287d8df46fc06c446bba7f513440cec786fad
איילת שילה <ashilo@g.jct.ac.il>
2024-11-12 22:40:09 +0200

    chapter 5,7a

---
+++ DalList/AssignmentImplementation.cs
@@ -0,0 +1,44 @@
+﻿
+namespace Dal;
+using DalApi;
+using DO;
+
+public class AssignmentImplementation : IAssignment
+{
+    public void Create(Assignment item)
+    {
+       //int NewId = DataSource.Config;
+       //Assignment newItem();
+       //newItem.Id = NewId;
+       //if (!Assignments.search)
+       // {
+       //     Assignments.pushback(newItem);
+       //     return NewId;
+       //}
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Assignment? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Assignment> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Assignment item)
+    {
+        throw new NotImplementedException();
+    }
+}
