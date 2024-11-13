00bad136f61e8e0f316865b050c2b54ad33a0c3c
tehilakufeld <tehilakufeld1@gmail.com>
2024-11-13 17:42:54 +0200

    config class, I hopet it is what we ate asked for.

---
+++ DalList/Config.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Dal;
+
+internal class Config
+{
+    internal const int startNextCallId = 1;
+    private static int NextCallId = startNextCallId;
+    internal static int nextCallId { get => NextCallId++; }
+
+
+    internal const int startNextAssignmentId = 1;
+    private static int NextAssignmentId = startNextAssignmentId;
+    internal static int nextAssignmentId { get => NextAssignmentId++; }
+
+    internal static DateTime Clock { get; set; } = DateTime.Now;
+
+    internal static void Reset()
+    {
+        NextCallId = startNextCallId;
+        NextAssignmentId = startNextAssignmentId;
+        Clock = DateTime.Now;
+    }
+
+}
