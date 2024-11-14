ed492a602e42112bb7f7eaf31f29d48d1d79c372
איילת שילה <ashilo@g.jct.ac.il>
2024-11-14 09:59:43 +0200

    step 10 try1

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Mail;
+﻿using DalApi;
+using System.Net.Mail;
 
 namespace DalTest
 {
@@ -6,35 +7,55 @@ namespace DalTest
     {
         static void Main(string[] args)
         {
-            
 
+          private static IConfig? s_dalConfig; //stage 1
+        private static ICall? s_dalCall; //stage 1
+        private static readonly Random s_rand = new();
+        private static void createCall()
+        {
 
-            private static void createCall()
+            string[] address =
+                    { "Hess 20,Haifa,Israel","Oren 19, Haifa,Israel","Moshe Sena 29, Jerusalem, Israel","Barzilai 5, Haifa,Israel","Nordau 43, Netanya,Israel"
+                    ,"Habanaei 27, Jerusalem, Israel","Ben Yehuda 112, Netanya, Israel","Rav Kook 22, Dimona, Israel","Hashomer 1, Tiberias, Israel","Rotem 15, Lod, Israel" };
+
+            for (int i = 0; i < 10; i++)
             {
 
+                string location = address[i];
 
-
-                for (int i = 0; i < 10; i++)
+                int rType = s_rand.Next(0, 3);
+                Type type;
+                switch (rType)
                 {
-                    string[] studentNames =
-                    { "Golda Meir 9 Hod Hasharon", "Erez 22 Metula", "Hrav Kook 12 Beer Sheva ", "Calnit 8 Natanya", "Hashalom 54 Jerusalem", "Hazit 5 Sdarot" ,};
+                    case 0:
+                        //
+                        break;
+                    case 1:
+                        //
+                        break;
+                    //
+                    case 2:
+                        //
+                        break;
 
-                    Type callType =
-                    string callAddress
-                        double latitude
-                        double longitude
-                        DateTime openTimen
-                        string? description =
-            DateTime ? maxTime =
-
-
-
-
-
-            s_dalCall!.Create(new());
+                    default:
+                        //
+                        break;
                 }
+
+                DateTime openTime = new DateTime(s_dalConfig.Clock; //stage 1
+                string? description =
+                DateTime ? maxTime =
+
+
+
+
+
+                  s_dalCall!.Create(new());
             }
         }
     }
 }
+    
+
 
