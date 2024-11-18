6602a8cfa9556933c385e3482beec85774660d21
tehilakufeld <tehilakufeld1@gmail.com>
2024-11-18 12:28:58 +0200

    dalList

---
+++ DalFacade/DalApi/IDal.cs
@@ -0,0 +1,11 @@
+﻿namespace DalApi;
+
+public interface IDal
+{
+    IAssignment Assignment { get; }
+    ICall Call { get; }
+    IVolunteer Volunteer { get; }
+    IConfig Config { get; }
+
+    void ResetDB();
+}
