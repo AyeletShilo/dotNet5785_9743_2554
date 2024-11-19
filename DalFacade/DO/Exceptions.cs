c617bcd285698a3dbb70bd1ed9ec403257e24bf5
איילת שילה <ashilo@g.jct.ac.il>
2024-11-19 10:36:04 +0200

    half of step 9

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -2,4 +2,22 @@
 
 namespace DO;
 
+[Serializable]
+public class DalDoesNotExistException : Exception
+{
+    public DalDoesNotExistException(string? message) : base(message) { }
+}
+
+public class DalAlreadyExistException : Exception
+{
+    public DalAlreadyExistException(string? message) : base(message) { }
+}
+
+
+public class DalDeletionImpossible : Exception
+{
+    public DalDeletionImpossible(string? message) : base(message) { }
+}
+
+
 
