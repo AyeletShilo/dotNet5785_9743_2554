577519eec8974502e375a8cec2ff584c7d57d07f
tehilakufeld <tehilakufeld1@gmail.com>
2024-11-26 20:57:17 +0200

    1-2 xml

--- DalFacade/DO/Exceptions.cs
+++ DalFacade/DO/Exceptions.cs
@@ -12,7 +12,10 @@ public class DalAlreadyExistException : Exception
     public DalAlreadyExistException(string? message) : base(message) { }
 }
 
-
+[Serializable]
+public class DalXMLFileLoadCreateException: Exception
+{    public DalXMLFileLoadCreateException(string? message) : base(message) { }
+}
 //[Serializable]
 //public class DalDeletionImpossible : Exception
 //{
