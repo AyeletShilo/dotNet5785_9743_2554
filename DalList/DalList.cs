ab5b5c47ff63a5a74488c5810fec2d3d6dc52c3f
tehilakufeld <tehilakufeld1@gmail.com>
2024-12-14 19:16:54 +0200

    Lazy singleton

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -1,8 +1,42 @@
 ﻿namespace Dal;
 using DalApi;
+//sealed internal class DalList : IDal
+//{
+//    public static IDal Instance { get; } = new DalList();
+//    private DalList() { }
+//    public IAssignment Assignment { get; } = new AssignmentImplementation();
+//    public ICall Call { get; } = new CallImplementation();
+//    public IVolunteer Volunteer { get; } = new VolunteerImplementation();
+//    public IConfig Config { get; } = new ConfigImplementation();
+
+//    public void ResetDB()
+//    {
+//        Assignment.DeleteAll();
+//        Call.DeleteAll();
+//        Volunteer.DeleteAll();
+//        Config.Reset();
+//    }
+//}
+
+
 sealed internal class DalList : IDal
 {
-    public static IDal Instance { get; } = new DalList();
+    private static IDal instance = null;
+    private static readonly object lockObject = new object();
+    
+    public static IDal Instance //Static constructor for the father
+    {
+        get
+        {
+            if (instance == null) //one check for singleton
+                lock (lockObject)
+                {
+                    if (instance == null) //second check for thread safe
+                        instance = new DalList(); //Creating an instance of DalList.
+                }
+            return instance;
+        }
+    }
     private DalList() { }
     public IAssignment Assignment { get; } = new AssignmentImplementation();
     public ICall Call { get; } = new CallImplementation();
--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -1,18 +1,50 @@
-﻿
-using DalApi;
-using System.Diagnostics;
-
+﻿using DalApi;
 namespace Dal;
+
+//sealed internal class DalXml : IDal
+//{
+//    public static IDal Instance { get; } = new DalXml();
+//    private DalXml() { }
+
+//    public IAssignment Assignment { get; } = new AssignmentImplementation();
+//    public ICall Call { get; } = new CallImplementation();
+//    public IVolunteer Volunteer { get; } = new VolunteerImplementation();
+//    public IConfig Config {  get; } = new ConfigImplementation();
+
+//    public void ResetDB()
+//    {
+//        Assignment.DeleteAll();
+//        Call.DeleteAll();
+//        Volunteer.DeleteAll();
+//        Config.Reset();
+//    }
+//}
+
+
 
 sealed internal class DalXml : IDal
 {
-    public static IDal Instance { get; } = new DalXml();
-    private DalXml() { }
+    private static IDal instance = null;
+    private static readonly object lockObject = new object();
 
+    public static IDal Instance //Static constructor for the father
+    {
+        get
+        {
+            if (instance == null) //one check for singleton
+                lock (lockObject)
+                {
+                    if (instance == null) //second check for thread safe
+                        instance = new DalXml(); //Creating an instance of DalList.
+                }
+            return instance;
+        }
+    }
+    private DalXml() { }
     public IAssignment Assignment { get; } = new AssignmentImplementation();
     public ICall Call { get; } = new CallImplementation();
     public IVolunteer Volunteer { get; } = new VolunteerImplementation();
-    public IConfig Config {  get; } = new ConfigImplementation();
+    public IConfig Config { get; } = new ConfigImplementation();
 
     public void ResetDB()
     {
@@ -22,6 +54,5 @@ sealed internal class DalXml : IDal
         Config.Reset();
     }
 }
-
 
 
