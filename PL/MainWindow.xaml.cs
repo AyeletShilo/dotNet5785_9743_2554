4cb5b2d004d54ed84063ed65ca9392ccc4419245
איילת שילה <ashilo@g.jct.ac.il>
2024-12-25 21:20:56 +0200

    step 5

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using PL.Volunteer;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +17,109 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        //public MainWindow()
+        //{
+        //    InitializeComponent();
+        //}
+
+
+        static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
+        public DateTime CurrentDate
         {
-            InitializeComponent();
+            get { return (DateTime)GetValue(CurrentDateProperty); }
+            set { SetValue(CurrentDateProperty, value); }
+        }
+        public static readonly DependencyProperty CurrentDateProperty =
+            DependencyProperty.Register("CurrentTime", typeof(DateTime), typeof(MainWindow));
+
+        private void btnAddOneMinute_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ForwardClock(BO.TimeUnit.Minute);
+        }
+        private void btnAddOneHour_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ForwardClock(BO.TimeUnit.Hour);
+        }
+        private void btnAddOneDay_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ForwardClock(BO.TimeUnit.Day);
+        }
+        private void btnAddOneMonth_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ForwardClock(BO.TimeUnit.Month);
+        }
+        private void btnAddOneYear_Click(object sender, RoutedEventArgs e)
+        {
+            s_bl.Admin.ForwardClock(BO.TimeUnit.Year);
+        }
+        public TimeSpan RiskRange
+        {
+            get { return (TimeSpan)GetValue(RiskRangeProperty); }
+            set { SetValue(RiskRangeProperty, value); }
+        }
+        public static readonly DependencyProperty RiskRangeProperty =
+            DependencyProperty.Register("CurrentTime", typeof(TimeSpan), typeof(MainWindow));
+        private void btnUpdateRiskRange_Click(TimeSpan riskRange, RoutedEventArgs e)
+        {
+            s_bl.Admin.SetMaxRange(riskRange);
+        }
+        private void clockObserver()
+        {
+            CurrentDate = s_bl.Admin.GetClock();
+        }
+
+        private void configObserver()
+        {
+            RiskRange = s_bl.Admin.GetMaxRange();
+        }
+
+        private void ScreenLoaded(object sender, RoutedEventArgs e) //?
+        {
+            CurrentDate = s_bl.Admin.GetClock();
+            RiskRange = s_bl.Admin.GetMaxRange();
+            s_bl.Admin.AddClockObserver(clockObserver);
+            s_bl.Admin.AddConfigObserver(configObserver);
+        }
+        private void ScreenClosed(object sender, RoutedEventArgs e) //?
+        {
+            s_bl.Admin.RemoveClockObserver(clockObserver);
+            s_bl.Admin.RemoveConfigObserver(configObserver);
+        }
+
+        private void btnVolunteer_Click(object sender, RoutedEventArgs e)
+        {
+            new VolunteerListWindow().Show();
+        }
+
+        private void btnResetDB_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Do you want to do reset?", "Click to confirm:", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result != MessageBoxResult.Yes) return;
+            CloseAllWindowsExceptMain();
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
+            s_bl.Admin.ResetDB();
+            Mouse.OverrideCursor = null;
+        }
+        private void btnInitializeDB_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Do you want to do an initializetion?", "Click to confirm:", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result != MessageBoxResult.Yes) return;
+            CloseAllWindowsExceptMain();
+            Mouse.OverrideCursor = System.Windows.Input.Cursors.Wait;
+            s_bl.Admin.InitializeDB();
+            Mouse.OverrideCursor = null;
+        }
+
+        private void CloseAllWindowsExceptMain()
+        {
+            foreach (Window window in Application.Current.Windows.Cast<Window>().ToList())
+                if (window != this)
+                    window.Close(); 
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
     }
 }
